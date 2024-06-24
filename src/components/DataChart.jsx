fecd06a160e519f9c314fc11688916c8bf141a97
Fleur <i506349@fhict.nl>
2024-06-24 18:24:07 +0200

    datachart testiiiiiiiiiiiiiiiiiAAAAAAAA

--- src/components/DataChart.jsx
+++ src/components/DataChart.jsx
@@ -165,6 +165,16 @@ const DataChart = () => {
             },
             y: {
               stacked: true,
+              beginAtZero: true,
+              ticks: {
+                callback: function (value) {
+                  // Custom labels for the y-axis
+                  if (value === 0) return "Low";
+                  if (value === 1) return "Medium";
+                  if (value === 2) return "High";
+                  return value; // Default to the actual value if it doesn't match
+                },
+              },
             },
           },
         },

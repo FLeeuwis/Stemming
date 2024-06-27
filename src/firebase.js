b37c9daf6bd6d47e86c10b692e0f199080a60729
Fleur <i506349@fhict.nl>
2024-06-27 22:49:55 +0200

    nog even en ik ga mij uitschrijven

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 import {
   doc,
@@ -27,6 +28,6 @@ const app = initializeApp(firebaseConfig);
 
 export default app;
 export const db = getFirestore(app);
-
+export const auth = getAuth(app);
 // Exporting everything that we need from firebase
 export { doc, onSnapshot, addDoc, collection, query, updateDoc, deleteDoc };
